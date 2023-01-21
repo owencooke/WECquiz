73f916588c53b951dedfefe2da82c8fc95996bfc
Owen Cooke <ocooke@ualberta.ca>
2023-01-20 21:27:26 -0800

    Best attempt

--- quiz/backend/server.py
+++ quiz/backend/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import random
 import openpyxl
+import json
+from jsonConverter import jsonToSQL
   
 # Init app
 app = Flask(__name__)
@@ -14,8 +16,15 @@ def init_quiz():
     # Init new quiz
     quiz = {
         "showProgressBar": "top",
+        "firstPageIsStarted": True,
+        "completedHtml": "<h4>You got <b>{correctAnswers}</b> out of <b>{questionCount}</b> correct answers.</h4>",
         "pages": [{
-            "elements": [{
+            "elements": [
+                {
+                "type": "html",
+                "html": "<h1>Quiz Time! Our Pacific: From Sea to Sky</h1>Enter your name below and click <b>Start Quiz</b> to begin."
+                },
+                {
                 "name": "Your name?",
                 "title": "Your name?",
                 "type": "text",
@@ -46,29 +55,36 @@ def init_quiz():
                     "name": row[0],
                     "title": row[0],
                     "type": "radiogroup",
+                    "correctAnswer": row[1],
                     "choices": choices,
                     "isRequired": True
                 }]
             })
 
     # #DEBUG: output init to json file for testing
-    # # Serializing json
-    # json_object = json.dumps(quiz, indent=2)
-    # # Writing to sample.json
-    # with open("sample.json", "w") as outfile:
-    #     outfile.write(json_object)
+    # j = json.dumps(quiz, indent=2)
+    # with open("quiz.json", "w") as outfile:
+    #     outfile.write(j)
             
     # generated quiz questions
     return quiz
   
 '''
 Return quiz results in form of JSON file. 
+and calculate quiz score / 10
 '''
-@app.route('/complete', methods=['POST'])
+@app.route('/complete', methods=['GET', 'POST'])
 def complete():
-    answers = request.get_json()
-    print(answers)
-    return 'Done', 201
-      
+    if request.method == 'POST':
+        # get their answers
+        global answers
+        answers = request.get_json()
+        jsonToSQL(answers)
+        # #DEBUG: output init to json file for testing
+        # j = json.dumps(answers, indent=2)
+        # with open("answers.json", "w") as outfile:
+        #     outfile.write(j)
+        return 'Done', 201
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- quiz/src/App.js
+++ quiz/src/App.js
@@ -6,17 +6,11 @@ import { Survey } from 'survey-react-ui';
 StylesManager.applyTheme("defaultV2");
 
 function App() {
-    // usestate for setting a javascript
-    // object for storing and using data
+    // load questions
     const [questions, setQuestions] = useState({});
-
-    // Using useEffect for single rendering
     useEffect(() => {
-        // Using fetch to fetch the api from 
-        // flask server it will be redirected to proxy
         fetch("/questions").then((res) =>
             res.json().then((questions) => {
-                // Setting a data from api
                 setQuestions(questions)
             })
         );
@@ -25,19 +19,17 @@ function App() {
     // Create survey model
     const survey = new Model(questions);
 
-    const alertResults = useCallback((sender) => {
-        const response = fetch("/add_todo", {
+    // Send user results to server
+    const sendResults = useCallback((sender) => {
+         fetch("/complete", {
             method: "POST",
             headers: {
             'Content-Type' : 'application/json'
             },
             body: JSON.stringify(sender.data)
             })
-            if (response.ok){
-            console.log("it worked")}
     }, []);
-
-    survey.onComplete.add(alertResults);
+    survey.onComplete.add(sendResults);
 
     return <Survey model={survey} />;
 }
